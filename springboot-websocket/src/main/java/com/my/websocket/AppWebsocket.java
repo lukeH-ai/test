dd16722f1ce3ccec7fa0f347e15c37ac1fe3d65a
luke <luke3863snow@gmial.com>
2020-07-26 17:08:26 +0800

    first commit

---
+++ springboot-websocket/src/main/java/com/my/websocket/AppWebsocket.java
@@ -0,0 +1,11 @@
+package com.my.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebsocket {
+    public static void main(String[] args) {
+        SpringApplication.run(AppWebsocket.class, args);
+    }
+}
---
+++ springboot-websocket/src/main/java/com/my/websocket/controller/WebController.java
@@ -0,0 +1,27 @@
+package com.my.websocket.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 跳转页面使用的控制层controller
+ */
+@Slf4j
+@Controller
+public class WebController {
+    @RequestMapping("/websocket/{name}")
+    public String webSocket(@PathVariable String name, Model model){
+        try{
+            log.info("跳转到websocket的页面上");
+            model.addAttribute("username",name);
+            return "websocket";
+        }
+        catch (Exception e){
+            log.error("跳转到websocket的页面上发生异常，异常信息是："+e.getMessage());
+            return "error";
+        }
+    }
+}
